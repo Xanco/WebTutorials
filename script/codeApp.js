32fcb014c6c764664c5ab6bd2c6f91e4e36e092a
James Elmour <james.elmour@gmail.com>
2014-08-25 18:49:46 +0100

    First commit

---
+++ script/codeApp.js
@@ -0,0 +1,92 @@
+﻿var mainApp = angular.module('mainApp', []);
+
+mainApp.controller('mainApp', function ($scope) {
+    $scope.Script = [
+        {
+            "snippet": "function(){ \n a = 12; \n}",
+            "comment": "Comment",
+            "filename": "javascript.js",
+            "language": "javascript"
+        },
+        {
+            'snippet': 'function awesome! (){ \n a = 1;\n} function awesome!! (){\nb = 2;\n}',
+            'comment': 'This code does something',
+            "filename": "javascript.js",
+            "language": "javascript"
+        },
+        {
+            'snippet': 'This is really cool code! And this is a added bonus!',
+            'comment': 'Adding to the previous code, this does something too',
+            "filename": "javascript.js",
+            "language": "javascript"
+        },
+        {
+            'snippet': '&lt;h1&gt;HTML!!!&lt;/h1&gt;',
+            'comment': 'Wooooooooooo!',
+            "filename": "index.html",
+            "language": "markup"
+        },
+    ];
+
+    $scope.coding = true;
+
+    $scope.currentScript = {
+        "snippet": $scope.Script[0].snippet,
+        "comment": $scope.Script[0].comment,
+        "language": $scope.Script[0].language
+    };
+
+    $scope.globalInfo = {
+        "title": "Writing to console in JavaScript",
+        "coding": true,
+        "files": ["index.html", "javascript.js"]
+    };
+
+    step = 0;
+    $scope.nextScript = function () {
+        if (step < objLengthAccurate($scope.Script)) {
+            step = step + 1;
+            $scope.getScript();
+            Prism.highlightAll();
+        }
+    };
+
+    $scope.prevScript = function () {
+        if (step != 0) {
+            step = step - 1;
+            $scope.getScript();
+            Prism.highlightAll();
+        }
+    };
+
+    $scope.getScript = function () {
+        $("#codeView").html($scope.Script[step].snippet);
+        $scope.currentScript.comment = $scope.Script[step].comment;
+        $scope.currentScript.language = $scope.Script[step].language;
+        $scope.currentStep();
+    };
+
+    $scope.currentStep = function () {
+        $("#codeCurrentStep").text((step + 1).toString() + "/" + objLength($scope.Script));
+    };
+});
+
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37:
+            $("#codePrev").click();
+            break;
+        case 39:
+            $("#codeNext").click();
+            break;
+
+    }
+});
+
+$(".codePanel").on("change", function () {
+    console.log("CHANGED!");
+    Prism.highlightAll();
+});
+
+
+Prism.highlightAll();
---
+++ script/globalFunctions.js
@@ -0,0 +1,15 @@
+﻿objLength = function (obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+objLengthAccurate = function (obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size - 1;
+};
